ce5c140857c8c10cb62d9eaa17cbd3b225a537c3
Fernando Castillo <jfnando_cas_30_@hotmail.com>
2016-02-14 14:19:20 -0500

    Finalizadooo

--- ProyectoProgV/ProyectoProgV/Datos/Conexion.cs
+++ ProyectoProgV/ProyectoProgV/Datos/Conexion.cs
@@ -4,15 +4,21 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProyectoProgV.Properties;
+using System.Configuration;
 
 namespace ProyectoProgV
 {
     class Conexion
     {
+        public static string obtenerString()
+        {
+            return Settings.Default.BDNutrilConnectionString;
+        }
 
         public static SqlConnection obtenerConexion()
         {
-            SqlConnection con = new SqlConnection("Data source=PC; Initial Catalog=BDNutril; Integrated Security=SSPI");
+            SqlConnection con = new SqlConnection(obtenerString());
             con.Open();
 
             return con;
